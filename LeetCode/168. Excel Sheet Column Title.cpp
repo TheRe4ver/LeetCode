2c2d1462b90defc275ef475e71be51e31d83d364
RE4VER <Podr_tj30@student.itstep.org>
2022-11-28 09:50:44 +0200

    not solved but still exist

--- LeetCode/168. Excel Sheet Column Title.cpp
+++ LeetCode/168. Excel Sheet Column Title.cpp
@@ -6,9 +6,9 @@ using namespace std;
 class Solution 
 {
 public:
-    int addDigits(int_fast64_t number)
+    int addDigits(uint_fast64_t number)
     {
-        int_fast64_t temp = 0;
+        uint_fast64_t temp = 0;
         while (number > 9)
         {
             while (number)
---
+++ LeetCode/202. Happy Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isHappy(int number)
+    {
+		int temp = number;
+		int sum = 0;
+		
+		while (true)
+		{
+			 temp = number % 10;
+			 int square = temp * temp;
+		}
+};
+
+void main()
+{
+
+}
