f36949c1d1cdc4d16ce36a45b4070c779ee0a70f
RE4VER <Podr_tj30@student.itstep.org>
2022-12-04 00:34:15 +0200

    solved

---
+++ LeetCode/268. Missing Number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution
+{
+public:
+	int missingNumber(vector<int>& nums)
+	{
+		int result = 0;
+		int size = nums.size();
+		sort(nums.begin(), nums.end());
+
+		for (int i = 0; i < size; i++)
+		{
+			if (i != nums[i])
+			{
+				return i;
+			}
+		}
+		return size;
+	}
+};
+
+void main()
+{
+	Solution program;
+	vector<int> nums{ 9,6,4,2,3,5,7,0,1 };
+	cout << program.missingNumber() << endl;
+}
