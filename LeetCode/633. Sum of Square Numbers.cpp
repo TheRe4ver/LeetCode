b8a804ea8206cdbe1e1c4d25ccbdf1f5c77e79d5
RE4VER <Podr_tj30@student.itstep.org>
2022-12-04 12:47:01 +0200

    solved

---
+++ LeetCode/633. Sum of Square Numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool judgeSquareSum(int target) 
+    {
+        int_fast64_t left = 0, right = sqrt(target);
+        while (left <= right)
+        {
+            if (left * left + right * right == target)
+                return true;
+            else if (left * left + right * right > target)
+                right--;
+            else
+                left++;
+        }
+        return false;
+    }
+};
+
+void main()
+{
+
+}
