dea7f1b9899c4100dd71e125dc68df3781ced1ca
RE4VER <Podr_tj30@student.itstep.org>
2022-12-04 12:10:52 +0200

    solved

---
+++ LeetCode/2256. Minimum Average Difference.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums)
+    {
+
+        int maxi = INT_MAX, res = 0, n = nums.size();
+
+        long total = 0, sum = 0;
+
+        for (auto x : nums)
+        {
+            total += x;
+        }
+        for (int i = 0; i < n - 1; i++) 
+        {
+
+            sum += nums[i];
+
+            long curr = abs(sum / (i + 1) - (total - sum) / (n - i - 1));
+
+            if (curr < maxi) 
+            {
+                maxi = curr;
+                res = i;
+            }
+        }
+
+        return maxi > total / n ? n - 1 : res;
+
+    }
+};
+
+void main()
+{
+    Solution program;
+    vector <int> nums{ 2,5,3,9,5,3 };
+    cout << program.minimumAverageDifference(nums) << endl;
+}
