9acb768cbc5428669b15e126ed03893139e5e3ac
TheRe4ver <gogytut@gmail.com>
2022-12-05 12:59:31 +0200

    fix and solved

--- LeetCode/876. Middle of the Linked List.cpp
+++ LeetCode/876. Middle of the Linked List.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 class Solution
 {
 public:
@@ -13,3 +26,10 @@ public:
         return slow;
     }
 };
+
+void main()
+{
+    Solution program;
+    ListNode* list = new ListNode();
+    ListNode* middle = program.middleNode(list);
+}
