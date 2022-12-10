f0c6576a8a84baf8a90ad9a55f67d8a2d92b6ca7
TheRe4ver <gogytut@gmail.com>
2022-12-10 10:55:36 +0200

    started

---
+++ LeetCode/10. Regular Expression Matching.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+	bool isMatch(string input, string pattern)
+	{
+		
+	}
+};
+
+void main()
+{
+	Solution program;
+	string input = "ab", pattern = ".*";
+	if (program.isMatch(input, pattern))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+}
