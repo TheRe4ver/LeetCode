bee91ca2645fb155697ba3990075e4c347ab21de
TheRe4ver <gogytut@gmail.com>
2022-12-10 19:22:54 +0200

    solved

--- LeetCode/10. Regular Expression Matching.cpp
+++ LeetCode/10. Regular Expression Matching.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-
+#include <string>
+#include <regex>
 using namespace std;
 
-class Solution
+class Solution 
 {
 public:
-	bool isMatch(string input, string pattern)
+	bool isMatch(string data, string pattern) 
 	{
-		
+		regex check(pattern);
+		return regex_match(data, check);
 	}
 };
 
 void main()
 {
 	Solution program;
-	string input = "ab", pattern = ".*";
-	if (program.isMatch(input, pattern))
-		cout << "Yes" << endl;
-	else
-		cout << "No" << endl;
+		string input = "ab", pattern = ".*";
+		if (program.isMatch(input, pattern))
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
 }
